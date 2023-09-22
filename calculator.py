2d298043de6ef31ca53c6796633d4be588d46d6a
serrayon <49975993+serrayon@users.noreply.github.com>
2023-09-21 20:55:40 -0700

    Update calculator.py
    added fib feature

--- calculator.py
+++ calculator.py
@@ -2,45 +2,70 @@
 Calculator library containing basic math operations
 """
 
-class Calculator:
-    def add(self,a,b):
-        return a+b 
-    def subtract(self,a,b):
-        return a-b
-    def multiply(self,a,b):
-        return a*b
-    def divide(self,a,b):
-        return a/b
-        
+class Calculator():
+    def __init__(self):
+        self.cache = [0, 1]  # Initialize the cache in the constructor
+
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        return a / b
+
+    def fib(self, n):
+        if not isinstance(n, int) or n < 0:
+            raise TypeError('Invalid input for Fibonacci sequence')
+        if n < len(self.cache):
+            return self.cache[n]
+        else:
+            while len(self.cache) <= n:
+                fib_number = self.cache[-1] + self.cache[-2]
+                self.cache.append(fib_number)
+            return self.cache[n]
+
+    def __call__(self, n):
+        if not isinstance(n, int) or n <= 0:
+            raise TypeError('Invalid input')
+        # Implement calculator functionality here
+        # ...
+
 calc = Calculator()
 
-#while loop 
 while True:
-    
-    #print statements
-    print('1 add')
-    print('2 subtract')
-    print('3 multiply')
-    print('4 divide')
-    print('5 exit')
+    print('1 Add')
+    print('2 Subtract')
+    print('3 Multiply')
+    print('4 Divide')
+    print('5 Fib')
+    print('6 Exit')
 
-    #input for function
-    #choice variable 1-5 exit
     choice = float(input('Choose a function: '))
-    #check if choice is valid
-    if choice in range(0,6):
-        #if choice == 5 break
-        if choice == 5:
+    if choice in range(1, 7):
+        if choice == 6:
             break
-        a = float(input('First number: '))
-        b = float(input('Second number: '))
+
+        if choice in [1, 2, 3, 4]:
+            a = float(input('First number: '))
+            b = float(input('Second number: '))
+
         if choice == 1:
-            print(a, "+", b, "=", calc.add(a,b))
+            print(a, '+', b, '=', calc.add(a, b))
         elif choice == 2:
-            print(a, "-", b, "=", calc.subtract(a,b))
+            print(a, '-', b, '=', calc.subtract(a, b))
         elif choice == 3:
-            print(a, "*", b, "=", calc.multiply(a,b))
+            print(a, '*', b, '=', calc.multiply(a, b))
         elif choice == 4:
-            print(a, "/", b, "=", calc.divide(a,b))
+            print(a, '/', b, '=', calc.divide(a, b))
+        elif choice == 5:
+            n = int(input('Enter a non-negative integer for Fibonacci: '))
+            print(f'The fib of {n} =', calc.fib(n))
+        else:
+            print('ERROR')
     else:
-        print('error')
+        print('Invalid choice, please select a valid option (1-6)')
